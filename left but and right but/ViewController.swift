83b4101838a9a9ea4e53b9dfbdb554ad646b2680
D7703_06 <d7703_06@D7703-06ui-iMac.local>
2018-03-19 14:10:20 +0900

    수정

--- left but and right but/ViewController.swift
+++ left but and right but/ViewController.swift
@@ -7,9 +7,9 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var lable: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +19,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBOutlet weak var left1: UIButton!
+    @IBAction func left(_ sender: UIButton) {
+        
+        let btnTitle = left1.title(for: .normal)
+        lable.text = btnTitle! + " Button Pressed"
+    }
+    
+    @IBOutlet weak var right1: UIButton!
+    @IBAction func right(_ sender: UIButton) {
+        let btnTitle = right1.title(for: .normal)
+        lable.text = btnTitle! + " Button Pressed"
+    }
+    
+    
 
 }
 
